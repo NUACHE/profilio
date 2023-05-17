69624e01dbdeccf5f13ad12203b36687980b7603
Michael Opoku <michaelopoku790@gmail.com>
2023-05-17 19:05:37 +0000

    changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,39 @@
-
+'use client'
 
 import Head from "next/head";
 import Image from "next/image";
 import AnimatedText from "../components/AnimatedText";
 import Layout from "../components/Layout";
 import profilePic from "../../public/images/profile/developer-pic-2.jpg"
+import { useEffect, useRef } from "react";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+
+const AnimatedNumbers = ({value}:any) => {
+    const ref = useRef<any>(null);
+    const motionValue = useMotionValue(0);
+    const springValue = useSpring(motionValue, {duration: 3000})
+    const isInView = useInView(ref, {once: true});
+
+    useEffect(()=>{
+        if(isInView){
+            motionValue.set(value);
+        }
+    },[isInView, value, motionValue])
+    
+    useEffect(()=>{
+      springValue.on("change", (latest)=>{
+       if(ref.current && latest.toFixed(0) <= value){
+        ref.current.textContent = latest.toFixed(0)
+       }
+      })
+    },[springValue, value])
+    return ( <span ref={ref}>
+
+    </span> );
+}
+ 
+
+
 
 const About = () => {
     return ( 
@@ -43,13 +72,29 @@ every project I work on. I look forward to the opportunity to bring my skills an
         <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
         <Image src={profilePic} alt="MO" className="w-full h-auto rounded-2xl" />
         </div>
-        <div>
-            <div>
-                <span>
-                    50+
+        <div className="col-span-2 flex flex-col items-end justify-between">
+            <div className="flex flex-col items-end justify-center ">
+                <span className="inline-block text-7xl font-bold">
+                   <AnimatedNumbers value={50} />+
                 </span>
-                <h2>
-                    satisfied
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                    satisfied clients
+                </h2>
+            </div>
+            <div className="flex flex-col items-end justify-center ">  
+                <span className="inline-block text-7xl font-bold">
+                <AnimatedNumbers value={40} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                    projects completed
+                </h2>
+            </div>
+            <div className="flex flex-col items-end justify-center ">
+                <span className="inline-block text-7xl font-bold">
+                <AnimatedNumbers value={4} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                   years of experience
                 </h2>
             </div>
             </div>     
