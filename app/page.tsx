7d1dea90c9b91a6e1b13f297d88b231eac326ca5
Michael Opoku <michaelopoku790@gmail.com>
2023-06-14 15:11:33 +0000

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
                 Explore my latest projects on github, showcasing my expertise in React.js and web development.</p>
 
               <div className="flex items-center self-start mt-2 lg:self-center">
-                <Link href="/Michael_Opoku_FE_Resume.pdf" target="_blank" className="dark:text-dark dark:bg-light  flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
+                <Link href="/Michael_Opoku_Resume.pdf" target="_blank" className="dark:text-dark dark:bg-light  flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
   hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base
   "
                   download={true}
